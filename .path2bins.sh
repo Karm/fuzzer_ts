812c029277b403fa0e7cfae42bda6d054834960a
Pavel Reichl <preichl@redhat.com>
2018-09-14 10:08:49 +0200

    Add script with paths to bins and patches

---
+++ .path2bins.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+AFL_GCC=`find -name afl-gcc`
+AFL_GCC=`realpath $AFL_GCC`
+PATCH_PATH=`find -name apatching_apache_for_AFL_fuzzing.diff`
+PATCH_PATH=`realpath ${PATCH_PATH}`
+PATCH_PATH2=`find -name patch2.diff`
+PATCH_PATH2=`realpath ${PATCH_PATH2}`
+
+
+for VAR in $AFL_GCC $PATCH_PATH $PATCH_PATH2; do
+    if [ ! -f "${VAR}" ]; then
+        echo "${VAR} is supposed do exist"
+        exit 1
+    fi
+done
